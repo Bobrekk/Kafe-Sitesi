c05429ba644f2998081b2f32638b95c8c1e8836a
Bobrekk <81027034+Bobrekk@users.noreply.github.com>
2023-05-12 17:23:56 +0300

    100+ Files

---
+++ Projet de Café/Projet de Café/My Project/AssemblyInfo.vb
@@ -0,0 +1,34 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' Bir derlemeyle ilgili Genel Bilgiler aşağıdaki öznitelik kümesi kullanılarak 
+' denetlenir. Bir derlemeyle ilişkilendirilmiş bilgileri değiştirmek için bu
+' öznitelik değerlerini değiştirin.
+
+' Derleme özniteliklerinin değerlerini incele
+<Assembly: AssemblyTitle("Projet_de_Café")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("Projet_de_Café")>
+<Assembly: AssemblyCopyright("Copyright ©  2022")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'Aşağıdaki GUID, bu proje COM'a gösterildiğinde typelib Kimliği içindir.
+<Assembly: Guid("63d2f1b5-b4b3-4666-9086-693dea40523c")>
+
+' Bir derlemenin sürüm bilgisi aşağıdaki dört değerden oluşur:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' Tüm değerleri belirtebilirsiniz veya Yapı ve Düzeltme Numaralarına aşağıda 
+' gösterildiği gibi '*' ile varsayılan değer atayabilirsiniz:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
